dab3c94cc6ef0a60e26c7dfaf9a1e291a278300b
DraconicVision <46660232+DraconicVision@users.noreply.github.com>
2021-04-12 21:09:52 +0100

    further syntax updates

--- dome.js
+++ dome.js
@@ -1,55 +1,54 @@
 var sites = {
-  'Entertainment': {
-    'YouTube (y)': 'https://www.youtube.com/',
-    'HBO': 'https://dk.hbonordic.com',
-    'Netflix': 'https://www.netflix.com/',
-    '9anime': 'https://9anime.ru/',
+  "Entertainment": {
+    "YouTube (y)" : "https://www.youtube.com/",
+    "HBO" : "https://dk.hbonordic.com",
+    "Netflix" : "https://www.netflix.com/",
+    "9anime" : "https://9anime.ru/",
   },
-  'MouseMods': {
-    'MouseMods.EU': 'https://www.mousemods.eu/',
-    'MouseMods.US': 'https://www.mousemods.us/',
-    'Shopify': 'https://mousemods.myshopify.com/',
-    'Mailerlite': 'https://app.mailerlite.com/',
+  "MouseMods" : {
+    "MouseMods.EU" : "https://www.mousemods.eu/",
+    "MouseMods.US" : "https://www.mousemods.us/",
+    "Shopify" : "https://mousemods.myshopify.com/",
+    "Mailerlite" : "https://app.mailerlite.com/",
   },
-  'E-Mail': {
-    'GMail': 'mail.google.com/mail/u/0/',
-    'Alibaba': 'https://message.alibaba.com/message/messenger.htm#/',
-    'Aliexpress':
-      'https://msg.aliexpress.com/?spm=a2g0o.best.1000001.32.72ad2c25Wb8zuM&tracelog=ws_topbar',
-    '10 Minute Mail': 'https://10minutemail.com/',
+  "E-Mail": {
+    "GMail" : "mail.google.com/mail/u/0/",
+    "Alibaba" : "https://message.alibaba.com/message/messenger.htm#/",
+    "Aliexpress" : "https://msg.aliexpress.com/?spm=a2g0o.best.1000001.32.72ad2c25Wb8zuM&tracelog=ws_topbar",
+    "10 Minute Mail" : "https://10minutemail.com/",
   },
   "General Utility": {
     // To find the game ID check the url in the store page or the community page
     //LastPass not working
-    "Google Drive": "https://drive.google.com/drive/",
-    "Google Docs": "https://docs.google.com/document/",
-    "Google Sheets": "https://docs.google.com/spreadsheets/",
-    "Google Forms": "https://docs.google.com/forms/",
+    "Google Drive" : "https://drive.google.com/drive/",
+    "Google Docs" : "https://docs.google.com/document/",
+    "Google Sheets" : "https://docs.google.com/spreadsheets/",
+    "Google Forms" : "https://docs.google.com/forms/",
   },
-  'Search': {
-    'Flaticon': 'https://www.flaticon.com',
-    'Image Search': 'https://yandex.com/images/',
-    'Adobe Images (a)': 'https://stock.adobe.com/dk/',
-    'Icon Search': 'https://thenounproject.com/',
-    'Mockups': 'https://mockups-design.com/',
-    'PNG EGG': 'https://www.pngegg.com/',
+  "Search": {
+    "Flaticon" : "https://www.flaticon.com",
+    "Image Search" : "https://yandex.com/images/",
+    "Adobe Images (a)" : "https://stock.adobe.com/dk/",
+    "Icon Search" : "https://thenounproject.com/",
+    "Mockups" : "https://mockups-design.com/",
+    "PNG EGG" : "https://www.pngegg.com/",
   },
-  'Social': {
-    'Reddit (r)': 'https://www.reddit.com/',
-    'Instagram': 'https://www.instagram.com/',
-    'Twitter': 'https://www.twitter.com/',
-    'Facebook': 'https://www.facebook.com/messages',
+  "Social": {
+    "Reddit (r)" : "https://www.reddit.com/",
+    "Instagram" : "https://www.instagram.com/",
+    "Twitter" : "https://www.twitter.com/",
+    "Facebook" : "https://www.facebook.com/messages",
   },
 };
 
 var search = {
   // Query variable name is q, hardcoded, looks like a standard already anyways
-  default: "https://google.com/search",
-  s: "https://startpage.com/do/search",
-  r: "https://reddit.com/search",
-  y: "https://youtube.com/search",
-  a: "https://stock.adobe.com/dk/search",
-  t: "https://translate.google.com/?hl=da",
+  "default": "https://google.com/search",
+  "s": "https://startpage.com/do/search",
+  "r": "https://reddit.com/search",
+  "y": "https://youtube.com/search",
+  "a": "https://stock.adobe.com/dk/search",
+  "t": "https://translate.google.com/?hl=da",
 };
 
 // ---------- BUILD PAGE ----------
